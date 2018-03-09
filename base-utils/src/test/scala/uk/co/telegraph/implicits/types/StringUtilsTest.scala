e4470ba868869ef135f2c1a7da9334cd0e793984
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2018-03-09 11:03:40 +0000

    responding to pr feedback

--- base-utils/src/test/scala/uk/co/telegraph/implicits/types/StringUtilsTest.scala
+++ base-utils/src/test/scala/uk/co/telegraph/implicits/types/StringUtilsTest.scala
@@ -1,6 +1,7 @@
 package uk.co.telegraph.implicits.types
 
-import org.scalatest.{FlatSpec, Matchers}
+import java.util.UUID
+import org.scalatest.{Assertion, FlatSpec, Matchers}
 import uk.co.telegraph.utils.implicits.types.StringUtils
 
 class StringUtilsTest
@@ -8,13 +9,16 @@ class StringUtilsTest
     with Matchers
     with StringUtils {
 
+  def stringAsUuidHasFailed(): Assertion = assert(false)
+  def stringAsUuidIsSuccessful(uuid: UUID): Assertion = assert(true)
+
   it should "implicitly convert a [[String]] to a [[UUID]] with the given success function" in {
     val input = "88a01bf3-8368-4ac0-b487-8d465414a797"
-    input.asUuid(uuid => uuid.toString shouldBe input)(assert(false))
+    input.asUuid(uuid => uuid.toString shouldBe input)(stringAsUuidHasFailed)
   }
 
   it should "implicitly convert a [[String]] to a [[UUID]] with the given failure function" in {
     val input = "this-is-an-invalid-uuid-string"
-    input.asUuid(_ => assert(false))(assert(true))
+    input.asUuid(stringAsUuidIsSuccessful)(stringAsUuidHasFailed)
   }
 }
