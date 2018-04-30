5454a19ec3a118cdd8bb81d4eb8ce558d813c66f
Adina Contiu <adina.contiu@3pillarglobal.com>
2018-04-30 10:34:10 +0300

    TTT-2376 Code review updates.

--- http-client/src/main/scala/uk/co/telegraph/utils/client/http/impl/SimpleHttpClient.scala
+++ http-client/src/main/scala/uk/co/telegraph/utils/client/http/impl/SimpleHttpClient.scala
@@ -4,6 +4,8 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.Materializer
+import org.json4s.Formats
+import org.json4s.jackson.JsonMethods
 import uk.co.telegraph.utils.client.models.ClientDetails
 
 import scala.concurrent.Future
@@ -33,21 +35,21 @@ class SimpleHttpClient(httpClient: HttpClient)(implicit val _actorSystem: ActorS
     } match {
       case Success(response) => response
       case Failure(exc) => throw new RejectedRequestException(httpRequest, exc)
-
-
     }
   }
 
   def getDetails(implicit timeout: FiniteDuration): Future[ClientDetails] = httpClient.getDetails
 }
 
-case class SimpleResponse(statusCode: Int, body: String)
+case class SimpleResponse(statusCode: Int, body: String) {
+  def bodyAs[A](implicit formats: Formats, mf: scala.reflect.Manifest[A]): A = JsonMethods.parse(body).extract[A]
+}
 
-class UnsuccessfulResponseException(val request: HttpRequest, val statusCode: Int, val responseBody: String)
+case class UnsuccessfulResponseException(val request: HttpRequest, val statusCode: Int, val responseBody: String)
   extends RuntimeException(s"An error occurred requesting $request, returned code $statusCode and body [$responseBody]")
 
-class RejectedRequestException(val request: HttpRequest, exception: Throwable)
+case class RejectedRequestException(val request: HttpRequest, exception: Throwable)
   extends RuntimeException(s"The http client rejected request $request and failed immediately", exception)
 
-class SimpleHttpClientException(val request: HttpRequest, exception: Exception)
+case class SimpleHttpClientException(val request: HttpRequest, exception: Exception)
   extends RuntimeException(s"An exception occurred while requesting $request", exception)
