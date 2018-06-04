435ec4275f126aff42b6b4debb0cda786b8b672d
AMCR <antonio.c.rodrigues@ist.utl.pt>
2018-06-04 15:36:52 +0100

    Fixing HttpInvalidStatus

--- http-client/src/main/scala/uk/co/telegraph/utils/client/http/exceptions/HttpInvalidStatusException.scala
+++ http-client/src/main/scala/uk/co/telegraph/utils/client/http/exceptions/HttpInvalidStatusException.scala
@@ -3,7 +3,7 @@ package uk.co.telegraph.utils.client.http.exceptions
 import akka.http.scaladsl.model.StatusCodes.InternalServerError
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCode}
 
-class HttpInvalidStatusException(message:String, cause:Throwable, statusCode:StatusCode = InternalServerError) extends HttpClientException(message, cause)
+case class HttpInvalidStatusException(message:String, cause:Throwable, statusCode:StatusCode = InternalServerError) extends HttpClientException(message, cause)
 
 object HttpInvalidStatusException{
 
@@ -11,5 +11,5 @@ object HttpInvalidStatusException{
     s"Request [${httpRequest.method.name} ${httpRequest.uri}] failed with $httpResponse. Cause: '${Option(cause).map(_.getMessage).getOrElse("-")}'"
 
   def apply(httpRequest: HttpRequest, httpResponse: HttpResponse, cause:Throwable = null, statusCode:StatusCode = InternalServerError):HttpClientException =
-    new HttpInvalidStatusException(errorMsgFormatter(httpRequest, httpResponse, cause), cause, statusCode)
+    HttpInvalidStatusException(errorMsgFormatter(httpRequest, httpResponse, cause), cause, statusCode)
 }
